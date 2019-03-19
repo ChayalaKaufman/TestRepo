c19c8ae2fcb880d7a54a1dcb6b4f48f8277dc431
Chayala Kaufman <chayalakaufman@gmail.com>
2019-03-18 21:52:33 -0400

    Add project files.

---
+++ classwork 031819 git/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork_031819_git
+{
+    public class Class1
+    {
+    }
+}
